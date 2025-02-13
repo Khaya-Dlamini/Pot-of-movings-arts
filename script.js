0772b74114be838872c2ca7bd8f3c6b10da503b1
Khayelihle <khayadlamini563@gmail.com>
2025-02-13 21:07:34 +0200

    made the banner page responsive

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ window.addEventListener("scroll", () => {
     }
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navMenu = document.querySelector(".nav-masthead");
+  
+    menuIcon.addEventListener("click", function () {
+      navMenu.classList.toggle("d-none"); // Show/hide menu
+      navMenu.classList.toggle("d-flex"); // Ensure flex styling when visible
+    });
+  });
+  
      
