f3f306b9add766ec3eaaa5ea0d9ed98a05db8eb3
Khayelihle <khayadlamini563@gmail.com>
2025-02-24 22:43:29 +0200

    added some lables for documentation

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-// window.addEventListener("scroll", () => {
-//     const header = document.querySelector("header");
-//     if (header) {
-//         header.classList.toggle("scrolling", window.scrollY > 0);
-//     }
-// }); 
-
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.querySelector(".menu-icon");
-    const navMenu = document.querySelector(".nav-masthead");
-  
-    menuIcon.addEventListener("click", function () {
-      navMenu.classList.toggle("d-none"); // Show/hide menu
-      navMenu.classList.toggle("d-flex"); // Ensure flex styling when visible
-    });
-  });
-  
-  
+  // menu-icon
   function toggleMenu() {
     document.querySelector(".mobile-nav").classList.toggle("active");
   }
@@ -26,3 +9,4 @@ document.querySelectorAll(".mobile-nav a").forEach(link => {
     document.querySelector(".mobile-nav").classList.remove("active");
   });
 });
+
